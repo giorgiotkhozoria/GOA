6f60e4e9a29e38b1fa2508305d9f1e5d28246bba
giorgiotkhozoria <giootkhozoria11@gmail.com>
2023-11-26 23:14:43 +0400

    magaria

---
+++ day1/homework.py
@@ -0,0 +1,6 @@
+name = "giorgi"
+surname = "Otkhozoria"
+age = 15
+height = 170,5
+hobby = "football"
+print("I am" + " " + (name) + " " + (surname) + " " + "i am " + str(age) + " years old" + " i am " + str (height) + " centimetres long " "and i love playing " + (hobby)) 
