93adeddd915884f8a802f8a69d115383426760d3
giorgiotkhozoria <giootkhozoria11@gmail.com>
2024-01-12 22:39:04 +0400

    qomiti

--- damatebitidvlb/dicegame/dicegame.py
+++ damatebitidvlb/dicegame/dicegame.py
@@ -1,19 +1,13 @@
-import random 
-a=input("Hello,Welcome to the dice game! To start enter your name.")
-d=0
-e=0
+import random
+inp = input("waht is ure name?: ")
+
 for i in range(6):
-    b=random.randint(1,6)
-    c=random.randint(1,6)
-    if b>c:
-            print("We roll the dice and!!! Looks like you won this round because",b," > ",c)
-            d+=1
-    elif c>b:
-            print("We roll the dice and!!! Looks like I won this round because",c," > ",b)
-            e+=1
-    else:
-            print("Its a draw!!",b," = ",c)
-    if d==3:
-            print("Looks like you won! Good job!")
-    elif e==3:
-            print("Looks like I won,Better luck next time!")
+        you = random.randint(1,6)
+        comp = random.randint(1,6)
+
+        if you > comp:
+                print(f"You won!! {inp}")
+        elif you < comp:
+                print(f"You loose!!{inp}")
+        else:
+                print('Draw{}'.format(inp))
