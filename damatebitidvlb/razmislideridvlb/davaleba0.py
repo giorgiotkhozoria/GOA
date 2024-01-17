816a5b5283b95808991d6d393d519176899aa5bc
giorgiotkhozoria <giootkhozoria11@gmail.com>
2024-01-17 22:53:24 +0400

    day 32

---
+++ damatebitidvlb/razmislideridvlb/davaleba0.py
@@ -0,0 +1,8 @@
+menu = ["xinkali", "mwvadi", "lobiani", "qababi", "khachapuri", "wyali",]
+print(menu[::-1])
+while menu in range(1):
+    menu[::-1]=menu[::1]
+    print(menu)
+
+scores = [20, 43, 56, 73, 10, 6, 87, 45 ,97]
+    
